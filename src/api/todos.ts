9002d87dd698ca31bc65fe8227fffc42ea12ad39
anton <anton@gmail.com>
2026-08-05 20:22:29 +0200

    Reactivee

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,25 +1,7 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const getUserId = () => {
-  if (typeof window === 'undefined') {
-    return 0;
-  }
-
-  try {
-    const user = localStorage.getItem('user');
-
-    if (!user) {
-      return 0;
-    }
-
-    return Number(JSON.parse(user).id) || 0;
-  } catch {
-    return 0;
-  }
-};
-
-export const USER_ID = getUserId();
+export const USER_ID = 4408;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
